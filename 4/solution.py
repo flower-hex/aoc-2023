393427e85d02d4d04bddc2b0a628e860a700ade2
lily <lily.holland@protonmail.com>
2023-12-11 18:33:12 +0000

    part 2 solution (easy??)

--- 4/solution.py
+++ 4/solution.py
@@ -1,15 +1,27 @@
 def card_reader(filename):
     with open(filename) as rawtxt:
         running_total = 0
+        card_index = -1
+        card_rolodex = []
         for line in rawtxt:
+            card_rolodex.append(0) #populate rolodex quickly
+        rawtxt.seek(0) #reset to start of file
+        for line in rawtxt:
+            card_index += 1
+            card_rolodex[card_index] += 1
             start_of_line = line.index(":") + 1
             winning_numbers, our_numbers = [x.split(" ") for x in line[start_of_line:-1].split("|")]
             overlap = len({x for x in winning_numbers if x != ""} & {x for x in our_numbers if x != ""}) #set intersection
             if overlap:
-                running_total += 2 ** (overlap - 1) #score doubles each overlap starting at 1 ie 2^(overlap-1)
-    return running_total
+                for i in range(1, overlap + 1):
+                    card_rolodex[card_index + i] += 1 * card_rolodex[card_index]
+    return sum(card_rolodex)
 
-EXAMPLE_SOLUTION = 13
+PART2 = True
+if not PART2:
+    EXAMPLE_SOLUTION = 13
+else:
+    EXAMPLE_SOLUTION = 30
 if (test_solution := card_reader("example") == EXAMPLE_SOLUTION):
     print("passed test!")
     print("solution should be {}".format(card_reader("input")))

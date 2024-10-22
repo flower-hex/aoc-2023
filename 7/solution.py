6375a71816a4655c273d240e1bb9242ae660f875
lily <lily.smiles.sunward@gmail.com>
2024-10-22 16:15:31 +0100

    day 7 progress

--- 7/solution.py
+++ 7/solution.py
@@ -1,3 +1,45 @@
 def parseInput(path):
     with open(path) as rawtxt:
-        return
+        hands = []
+        for line in rawtxt:
+            a, b = line.split()
+            formattedline = {"cards" : a, "bid" : int(b)}
+            hands.append(formattedline)
+        return hands
+    
+def sortHandsQuickPass(hands):
+    roughSort = {
+        "Five of a kind" : [],      # AAAAA
+        "Four of a kind" : [],      # AA8AA
+        "Full house" : [],          # 23332
+        "Three of a kind" : [],     # TTT98
+        "Two pair" : [],            # 23432
+        "One pair" : [],            # A23A4
+        "High card" : []            # 23456
+        }
+    for hand in hands:
+        quickCount = len(set(c for c in hand["cards"]))
+        if quickCount == 5: # 5 different cards
+            roughSort["High card"].append(hand)
+        elif quickCount == 4: # 4 different
+            roughSort["One pair"].append(hand)
+        elif quickCount == 1:
+            roughSort["Five of a kind"].append(hand)
+        elif quickCount == 2: # four of a kind OR full house
+            if hand.count(hand[0]) in (1,4):
+                roughSort["Four of a kind"].append(hand)
+            else: roughSort["Full house"].append(hand)
+        else: # three of a kind OR two pair
+            assert quickCount == 3
+            for c in hand["cards"]: # first card 
+                cardCount = hand["cards"].count(c)
+                if cardCount == 2:
+                    roughSort["Two pair"].append(hand)
+                    break
+                elif cardCount == 3:
+                    roughSort["Three of a kind"].append(hand)
+                    break
+    return roughSort
+
+def refineSort(roughSort):
+    return
--- 7/solution_test.py
+++ 7/solution_test.py
@@ -1,4 +1,4 @@
-from solution import parseInput
+from solution import parseInput, sortHandsQuickPass, refineSort
 import pytest
 
 ''' example input:
@@ -11,8 +11,38 @@ QQQJA 483
 
 PATH_EXAMPLE = "7/example.txt"
 
-FORMATTED_INPUT_EXAMPLE = [{"hand" : ["3", "2","T","3","K"], "bid" : 765}]
+FORMATTED_INPUT_EXAMPLE = [{"cards" : "32T3K", "bid" : 765},
+                           {"cards" : "T55J5", "bid" : 684},
+                           {"cards" : "KK677", "bid" : 28},
+                           {"cards" : "KTJJT", "bid" : 220},
+                           {"cards" : "QQQJA", "bid" : 483}]
+
+ROUGH_SORT_EXAMPLE = {
+        "Five of a kind" : [],
+        "Four of a kind" : [],
+        "Full house" : [],
+        "Three of a kind" : [{"cards" : "T55J5", "bid" : 684}, {"cards" : "QQQJA", "bid" : 483}],
+        "Two pair" : [{"cards" : "KK677", "bid" : 28}, {"cards" : "KTJJT", "bid" : 220}],
+        "One pair" : [{"cards" : "32T3K", "bid" : 765}],
+        "High card" : []}
+
+FINE_SORT_EXAMPLE = {
+        "Five of a kind" : [],
+        "Four of a kind" : [],
+        "Full house" : [],
+        "Three of a kind" : [{"cards" : "QQQJA", "bid" : 483}, {"cards" : "T55J5", "bid" : 684}],
+        "Two pair" : [{"cards" : "KTJJT", "bid" : 220}, {"cards" : "KK677", "bid" : 28}],
+        "One pair" : [{"cards" : "32T3K", "bid" : 765}],
+        "High card" : []}
 
 def testParsing():
-    formattedInputAttempt = parseInput("32T3K 765")
+    formattedInputAttempt = parseInput(PATH_EXAMPLE)
     assert formattedInputAttempt == FORMATTED_INPUT_EXAMPLE
+
+def testRoughSort():
+    roughlySorted = sortHandsQuickPass(FORMATTED_INPUT_EXAMPLE)
+    assert roughlySorted == ROUGH_SORT_EXAMPLE
+
+def refineSort():
+    finelySorted = refineSort(ROUGH_SORT_EXAMPLE)
+    assert finelySorted == FINE_SORT_EXAMPLE

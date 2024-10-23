1c6b2187865ba1584b2e29f57b579de06497eb7f
lily <lily.smiles.sunward@gmail.com>
2024-10-23 19:35:50 +0100

    part 1 solve

--- 8/day8_solution.py
+++ 8/day8_solution.py
@@ -1,2 +1,34 @@
+STARTPOINT = "AAA"
+ENDPOINT = "ZZZ"
+INPUT_PATH = "8/input.txt"
+
 def parseInput(path):
-    return
+    with open(path) as rawtxt:
+        directions = []
+        network = {}
+        for c in rawtxt.readline():
+            if c == "L":
+                directions.append(0)
+            elif c == "R":
+                directions.append(1)
+        for line in rawtxt:
+            if "=" in line:
+                key, value = line.split(" = ")
+                value = value.strip("()\n")
+                values = tuple(value.split(", "))
+                node = {key : values}
+                network |= node
+        return (directions, network)
+    
+def traverseNetwork(directions, network, startpoint, endpoint):
+    currentNode = startpoint
+    steps = 0
+    while currentNode != endpoint:
+        for instruction in directions:
+            currentNode = network[currentNode][instruction]
+            steps += 1
+    return steps
+
+directions, network = parseInput(INPUT_PATH)
+solution = traverseNetwork(directions, network, STARTPOINT, ENDPOINT)
+print("day 8 solution is {}".format(solution))
--- 8/day8_test.py
+++ 8/day8_test.py
@@ -1,27 +1,37 @@
 import pytest
-from day8_solution import parseInput
+import day8_solution
 
-FORMATTED_INPUT_DIRECTIONS_EXAMPLE = [1,0] # no native bit field type sadface
+FORMATTED_DIRECTIONS_EXAMPLE = [1,0] # no native bit field type sadface
 
-FORMATTED_INPUT_NETWORK_EXAMPLE = {"AAA" : ("BBB", "CCC"),
-                                   "BBB" : ("DDD", "EEE"),
-                                   "CCC" : ("ZZZ", "GGG"),
-                                   "DDD" : ("DDD", "DDD"),
-                                   "EEE" : ("EEE", "EEE"),
-                                   "GGG" : ("GGG", "GGG"),
-                                   "ZZZ" : ("ZZZ", "ZZZ"),}
+FORMATTED_NETWORK_EXAMPLE = {"AAA" : ("BBB", "CCC"),
+                             "BBB" : ("DDD", "EEE"),
+                             "CCC" : ("ZZZ", "GGG"),
+                             "DDD" : ("DDD", "DDD"),
+                             "EEE" : ("EEE", "EEE"),
+                             "GGG" : ("GGG", "GGG"),
+                             "ZZZ" : ("ZZZ", "ZZZ"),}
 
-FORMATTED_INPUT_DIRECTIONS_EXAMPLE_2 = [0,0,1]
+FORMATTED_DIRECTIONS_EXAMPLE_2 = [0,0,1]
 
-FORMATTED_INPUT_NETWORK_EXAMPLE_2 = {"AAA" : ("BBB", "BBB"),
-                                     "BBB" : ("AAA", "ZZZ"),
-                                     "ZZZ" : ("ZZZ", "ZZZ")}
+FORMATTED_NETWORK_EXAMPLE_2 = {"AAA" : ("BBB", "BBB"),
+                               "BBB" : ("AAA", "ZZZ"),
+                               "ZZZ" : ("ZZZ", "ZZZ")}
+
+OUTPUT_EXAMPLE = 2
+OUTPUT_EXAMPLE_2 = 6
 
 @pytest.mark.parametrize("input,expected_directions,expected_network", [
-  ("8/example", FORMATTED_INPUT_DIRECTIONS_EXAMPLE, FORMATTED_INPUT_NETWORK_EXAMPLE),
-  ("8/example2", FORMATTED_INPUT_DIRECTIONS_EXAMPLE_2, FORMATTED_INPUT_NETWORK_EXAMPLE_2)
+  ("8/example.txt", FORMATTED_DIRECTIONS_EXAMPLE, FORMATTED_NETWORK_EXAMPLE),
+  ("8/example2.txt", FORMATTED_DIRECTIONS_EXAMPLE_2, FORMATTED_NETWORK_EXAMPLE_2)
 ])
 def test_input_format_directions(input,expected_directions,expected_network):
-    results = parseInput(input)
-    assert results == expected_directions, expected_network
+    results = day8_solution.parseInput(input)
+    assert results == (expected_directions, expected_network)
 
+@pytest.mark.parametrize("directions,network, expected_result", [
+  (FORMATTED_DIRECTIONS_EXAMPLE, FORMATTED_NETWORK_EXAMPLE, OUTPUT_EXAMPLE),
+  (FORMATTED_DIRECTIONS_EXAMPLE_2, FORMATTED_NETWORK_EXAMPLE_2, OUTPUT_EXAMPLE_2)
+])
+def test_traverseNetwork(directions,network, expected_result):
+    result = day8_solution.traverseNetwork(directions,network, day8_solution.STARTPOINT, day8_solution.ENDPOINT)
+    assert result == expected_result
